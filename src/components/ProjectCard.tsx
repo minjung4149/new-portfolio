804f8b0e9d0622692a1ca5778052178874b06bbb
minjung4149 <minjung4149@naver.com>
2025-01-02 16:58:41 +0900

    [feat] 프로젝트 섹션 컴포넌트 분리, 프로젝트 카드 컴포넌트 작성

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+interface ProjectCardProps {
+  imageSrc: string;
+  imageAlt: string;
+  link: string;
+  title: string;
+  description: string;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({imageSrc, imageAlt, link, title, description}) => {
+  return (
+    <>
+      <div className="img_wrap">
+        <img src={imageSrc} alt={imageAlt}/>
+      </div>
+      <div className="project_desc">
+        <div className="gradient_bg"></div>
+        <a href={link} className="txt" target="_blank" rel="noopener noreferrer">
+          <div className="tt_line f_b_c">
+            <h3>{title}</h3>
+            <div className="onclick_icon">
+              <img src="/images/onclick.svg" alt={title}/>
+            </div>
+          </div>
+          <p className="desc">{description}</p>
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default ProjectCard;
---
+++ src/components/section/Project.tsx
@@ -0,0 +1,63 @@
+'use client';
+import React, {useEffect, useState} from 'react';
+import Category from "@/components/Category";
+import ProjectCard from "@/components/ProjectCard";
+
+interface ProjectData {
+  imageSrc: string;
+  imageAlt: string;
+  link: string;
+  title: string;
+  description: string;
+};
+
+
+async function fetchProjects(): Promise<ProjectData[]> {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+  const res = await fetch(`${baseUrl}/data/projects.json`, {cache: 'no-store'});
+  if (!res.ok) {
+    throw new Error('Failed to fetch projects data');
+  }
+  return res.json();
+}
+
+const Project: React.FC = () => {
+  const [projects, setProjects] = useState<ProjectData[]>([]);
+  const lastRowStartIndex = Math.floor((projects.length - 1) / 3) * 3;
+
+  useEffect(() => {
+    const loadProjects = async () => {
+      try {
+        const data = await fetchProjects();
+        setProjects(data);
+      } catch (error) {
+        console.error('Error loading projects:', error);
+      }
+    };
+
+    loadProjects();
+  }, []);
+
+  return (
+    <section className="preview" id="project">
+      <Category title="Project"/>
+      <div className="work_wrapper common">
+        {projects.map((project, index) => {
+          // 마지막 줄 여부 확인
+          const isLastRow = index >= lastRowStartIndex;
+
+          return (
+            <div
+              key={index}
+              className={`project ${isLastRow ? "no-bottom-border" : ""}`}
+            >
+              <ProjectCard {...project} />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default Project;
